25ecf0a340d99fd543715cc81044831c677aa4a6
Ian Morgan <ian.j.morgan@gmail.com>
2021-11-09 09:08:12 +0000

    added equality to the spy

--- src/main/kotlin/mycorda/app/xunitpatterns/spy/Spy.kt
+++ src/main/kotlin/mycorda/app/xunitpatterns/spy/Spy.kt
@@ -5,6 +5,18 @@ package mycorda.app.xunitpatterns.spy
  */
 class Spy {
     private val secrets = ArrayList<String>()
-    fun spy(secret: String) = secrets.add(secret)
+    fun spy(secret: String): Spy {
+        secrets.add(secret)
+        return this
+    }
+
     fun secrets(): List<String> = secrets
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is Spy) secrets() == secrets() else false
+    }
+
+    override fun hashCode(): Int {
+        return secrets.hashCode()
+    }
 }
